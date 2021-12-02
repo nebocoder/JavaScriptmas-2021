c75e960afffbb15d4fa06324385e3dbfbf607393
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-02 16:58:04 +0100

    JS

--- 2-toggle-christmas-theme/index.js
+++ 2-toggle-christmas-theme/index.js
@@ -1,7 +1,5 @@
 const body = document.getElementById("body")
-const greeting = document.getElementById("greeting")
-const christmasBtn = document.getElementById("christmas")
-const snowBtn = document.getElementById("snow")
+const themeSwitch = document.getElementById("theme-switch")
 
 // Task:
 //- Add the functionality to switch the theme between 'Christmas' and 'snow'.
@@ -10,3 +8,13 @@ const snowBtn = document.getElementById("snow")
 // - Add more themes!
 // - Allow the user to customise the themes.
 // - Turn the radio buttons into a toggle switch.
+
+themeSwitch.addEventListener("change", () => {
+  if (body.classList.contains("christmas")) {
+    body.classList.add("snow")
+    body.classList.remove("christmas")
+  } else if (body.classList.contains("snow")) {
+    body.classList.add("christmas")
+    body.classList.remove("snow")
+  }
+})
