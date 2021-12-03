032dfe5ff04a6581695e834b2ddaad423aaaa0ce
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-03 17:04:40 +0100

    Basic functionality

--- 3-dont-scare-relatives/index.js
+++ 3-dont-scare-relatives/index.js
@@ -1,11 +1,21 @@
 const greeting = document.getElementById("greeting")
 const btn = document.getElementById("btn")
+const container = document.querySelector(".container")
+const body = document.querySelector("body")
+const hidden = document.querySelector(".hidden")
 
 function fix() {
   // Task:
   // - Write a function to fix the UI problems with this Christmas message (make it Christmassy!)
   // - Run the function when the Fix button is clicked.
+  hidden.style.display = "block"
+  container.style.fontFamily = "Mountains of Christmas"
+  container.style.background = 'url("img/trees.png")'
+  greeting.textContent = "🎅 Merry Christmas! 🎄"
+  body.style.background = 'url("img/christmas-dark.png")'
 }
+
+btn.addEventListener("click", fix)
 
 //Stretch goals:
 // - Add an extra theme, and the option to switch between them.
