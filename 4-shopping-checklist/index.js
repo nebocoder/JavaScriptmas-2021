598098bf74c7751d28170838c52d33204ba3e043
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-04 17:15:34 +0100

    Local storage

--- 4-shopping-checklist/index.js
+++ 4-shopping-checklist/index.js
@@ -1,10 +1,3 @@
-let items = []
-const checklist = document.getElementById("checklist")
-const addInput = document.getElementById("add-input")
-const addBtn = document.getElementById("add-btn")
-const deleteAllBtn = document.getElementById("delete-all-btn")
-let elementList = ""
-
 // Task:
 // - For each item in the items array, create a div with a class of "checklist-item", which contains a checkbox input and corresponding label.
 // - Make sure that the shopping list can render a checkbox for all the items, even if new items are added to the items array.
@@ -12,6 +5,21 @@ let elementList = ""
 // Stretch goals:
 // - Add an input which allows the user to add more items.
 // - Add a delete button for the items.
+
+const checklist = document.getElementById("checklist")
+const addInput = document.getElementById("add-input")
+const addBtn = document.getElementById("add-btn")
+const deleteAllBtn = document.getElementById("delete-all-btn")
+
+let items = []
+let elementList = ""
+
+let storedItems = JSON.parse(localStorage.getItem("items"))
+
+if (storedItems) {
+  items = storedItems
+  render()
+}
 
 function render() {
   elementList = ""
@@ -26,11 +34,13 @@ function render() {
 
 addBtn.addEventListener("click", () => {
   items.push(addInput.value)
+  localStorage.setItem("items", JSON.stringify(items))
   addInput.value = ""
   render()
 })
 
 deleteAllBtn.addEventListener("click", () => {
+  localStorage.clear()
   items = []
   checklist.innerHTML = ""
 })
