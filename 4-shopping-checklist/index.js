6b37a1f7381b20f634f7adece0efcf90a591c0d1
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-04 17:06:36 +0100

    Strikethrough

--- 4-shopping-checklist/index.js
+++ 4-shopping-checklist/index.js
@@ -8,12 +8,17 @@ let elementList = ""
 // Task:
 // - For each item in the items array, create a div with a class of "checklist-item", which contains a checkbox input and corresponding label.
 // - Make sure that the shopping list can render a checkbox for all the items, even if new items are added to the items array.
+
+// Stretch goals:
+// - Add an input which allows the user to add more items.
+// - Add a delete button for the items.
+
 function render() {
   elementList = ""
   items.forEach((item) => {
     elementList += `<div class="checklist-item">
               <input type="checkbox" name="item" />
-              <label for="item">${item}</label>
+              <label class="strikethrough" for="item">${item}</label>
             </div>`
   })
   checklist.innerHTML = elementList
@@ -25,10 +30,7 @@ addBtn.addEventListener("click", () => {
   render()
 })
 
-deleteAllBtn.addEventListener("dblclick", () => {
+deleteAllBtn.addEventListener("click", () => {
   items = []
   checklist.innerHTML = ""
 })
-// Stretch goals:
-// - Add an input which allows the user to add more items.
-// - Add a delete button for the items.
