5368d821d134d5d0a7535acb5657b3297d96ac35
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-06 19:16:23 +0100

    Styling

--- 6-text-christmassifier/index.js
+++ 6-text-christmassifier/index.js
@@ -1,5 +1,7 @@
 const greeting = document.getElementById("greeting")
 const christmassifierBtn = document.getElementById("christmassifierBtn")
+const body = document.querySelector("body")
+const container = document.querySelector(".container")
 christmassifierBtn.addEventListener("click", christmassifyName)
 
 function christmassifyName() {
@@ -7,10 +9,14 @@ function christmassifyName() {
   // - Add christmassify class to greeting.
   //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
   if (christmassifierBtn.textContent === "Christmassify") {
+    body.style.background = "url('img/christmas-colour.png')"
+    container.style.background = "url('img/trees.png')"
     greeting.classList.add("christmassified")
     christmassifierBtn.classList.add("christmassified")
     christmassifierBtn.textContent = "De-christmassify"
   } else if ((christmassifierBtn.textContent = "De-christmassify")) {
+    body.style.background = "var(--light-green)"
+    container.style.background = "var(--snow)"
     greeting.classList.remove("christmassified")
     christmassifierBtn.classList.remove("christmassified")
     christmassifierBtn.textContent = "Christmassify"
