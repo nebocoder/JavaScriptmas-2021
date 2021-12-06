68c0477556de533f7f18d522ded7d2c186023e50
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-06 19:03:52 +0100

    Basic functionality

--- 6-text-christmassifier/index.js
+++ 6-text-christmassifier/index.js
@@ -6,6 +6,15 @@ function christmassifyName() {
   // Task:
   // - Add christmassify class to greeting.
   //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
+  if (christmassifierBtn.textContent === "Christmassify") {
+    greeting.classList.add("christmassified")
+    christmassifierBtn.classList.add("christmassified")
+    christmassifierBtn.textContent = "De-christmassify"
+  } else if ((christmassifierBtn.textContent = "De-christmassify")) {
+    greeting.classList.remove("christmassified")
+    christmassifierBtn.classList.remove("christmassified")
+    christmassifierBtn.textContent = "Christmassify"
+  }
 }
 
 // Stretch goals:
