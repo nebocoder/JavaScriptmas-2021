4552f2904a84bc86f16bf124e9013343dbdd6347
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-07 17:15:23 +0100

    Basic functionality

--- 7-play-a-christmas-song/index.js
+++ 7-play-a-christmas-song/index.js
@@ -2,8 +2,23 @@ const playBtn = document.getElementById("play-btn")
 const pauseBtn = document.getElementById("pause-btn")
 const stopBtn = document.getElementById("stop-btn")
 
+const jingle = new Audio("jingle-bells.mp3")
+
 // Task:
 // - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
+
+playBtn.addEventListener("click", () => {
+  jingle.play()
+})
+
+pauseBtn.addEventListener("click", () => {
+  jingle.pause()
+})
+
+stopBtn.addEventListener("click", () => {
+  jingle.pause()
+  jingle.currentTime = 0
+})
 
 // Stretch goals:
 // - Add volume controls.
