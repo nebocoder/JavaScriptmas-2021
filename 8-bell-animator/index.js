8e5700b852bfb77b902efaaf0cf00bb53100dec8
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-08 17:16:59 +0100

    Basic functionality

--- 8-bell-animator/index.js
+++ 8-bell-animator/index.js
@@ -4,26 +4,31 @@ const stopBtn = document.getElementById("stop-btn")
 // Credit: https://www.freesoundslibrary.com/jingle-bells-track/
 const jingle = new Audio("jingle-bells.mp3")
 const volume = document.getElementById("volume")
+const bell = document.getElementById("bell")
 
 // Task:
-// - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
+// - Animate the bell so that it looks like it is ringing when the music is playing, and stops when the music is paused or stopped.
+
+//NB: You'll need to make changes in the CSS too 😉
+
+// Stretch goal:
+// - Make sure the animation doesn't reset when paused.
 
 playBtn.addEventListener("click", () => {
   jingle.play()
+  bell.classList.add("jingle")
 })
 
 pauseBtn.addEventListener("click", () => {
   jingle.pause()
+  bell.classList.remove("jingle")
 })
 
 stopBtn.addEventListener("click", () => {
   jingle.pause()
+  bell.classList.remove("jingle")
   jingle.currentTime = 0
 })
-
-// Stretch goals:
-// - Add volume controls.
-// - Allow the user to select different sounds.
 
 volume.addEventListener("input", () => {
   jingle.volume = volume.value / 100
