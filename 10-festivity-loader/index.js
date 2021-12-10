9b71426a41d3fd83324cb643164c3b15314a2921
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-10 16:44:07 +0100

    Basic functionality

--- 10-festivity-loader/index.js
+++ 10-festivity-loader/index.js
@@ -1,4 +1,5 @@
 const meter = document.getElementById("meter")
+const current = new Date().getDate()
 
 // Task:
 // Write a function to wire up the festivity loader to reflect how many days are remaining until Christmas!
@@ -6,3 +7,9 @@ const meter = document.getElementById("meter")
 // Stretch goals:
 // - Animate the loader.
 // - Change the colors depending on the meter's value.
+
+function loading() {
+  meter.value = current
+}
+
+loading()
