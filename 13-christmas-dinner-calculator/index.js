6cf6914fdf0aabe9132b0c0cfcf37cd89898818e
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-13 16:45:41 +0100

    Basic functionality

--- 13-christmas-dinner-calculator/index.js
+++ 13-christmas-dinner-calculator/index.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById("btn")
-let food = document.getElementById("food")
+const food = document.getElementById("food")
+const guestsInput = document.getElementById("guests-input")
+const vegetarianInput = document.getElementById("vegetarian-input")
 
 // Tasks:
 // - Write the JS to decide the perfect Christmas dinner and render it in the result element. Don't forget to check whether the meal should be vegetarian!
@@ -12,3 +14,19 @@ let food = document.getElementById("food")
 // Stretch goals:
 // - Add more dietary options.
 // - Show recipes when the meal has been selected.
+
+btn.addEventListener("click", selectMeal)
+
+function selectMeal() {
+  if (guestsInput.value === "0" || !guestsInput.value) {
+    alert("Please enter the number of guests.")
+  } else {
+    if (vegetarianInput.checked) {
+      food.textContent = "Mushroom Risotto!"
+    } else if (guestsInput.value <= 4) {
+      food.textContent = "Honey Roasted Pork Loin!"
+    } else if (guestsInput.value > 4) {
+      food.textContent = "Herb Roasted Turkey!"
+    }
+  }
+}
