3b2f00da530e8576b9ed639d54c0911ebd82701c
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-14 17:17:30 +0100

    Basic functionality

--- 14-lonely-elf/index.js
+++ 14-lonely-elf/index.js
@@ -1,5 +1,6 @@
 const elf = document.getElementById("elf")
 const btn = document.getElementById("btn")
+const elfZone = document.getElementById("elf-hangout-zone")
 
 btn.addEventListener("click", duplicateElf)
 
@@ -7,6 +8,7 @@ function duplicateElf() {
   // Task:
   // - Write a function to duplicate the elf when the button is clicked.
   // - See index.css for optional styling challenges.
+  elfZone.innerHTML += `<div id="elf" class="elf">🧝</div>`
 }
 
 // Stretch goals:
