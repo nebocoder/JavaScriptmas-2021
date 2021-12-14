9aede52522e3205a97a1ce37b84dbbd2e9c0d2c4
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-14 17:35:41 +0100

    Stretch goals

--- 14-lonely-elf/index.js
+++ 14-lonely-elf/index.js
@@ -1,16 +1,34 @@
 const elf = document.getElementById("elf")
-const btn = document.getElementById("btn")
+const elfBtn = document.getElementById("elfBtn")
+const teaBtn = document.getElementById("teaBtn")
+const deerBtn = document.getElementById("deerBtn")
 const elfZone = document.getElementById("elf-hangout-zone")
 
-btn.addEventListener("click", duplicateElf)
+elfBtn.addEventListener("click", addElf)
+teaBtn.addEventListener("click", addTea)
+deerBtn.addEventListener("click", addDeer)
 
-function duplicateElf() {
+function addElf() {
   // Task:
   // - Write a function to duplicate the elf when the button is clicked.
   // - See index.css for optional styling challenges.
-  elfZone.innerHTML += `<div id="elf" class="elf">🧝</div>`
+  if (elfZone.childElementCount < 101) {
+    elfZone.innerHTML += `<div class="elf">🧝</div>`
+  }
 }
 
 // Stretch goals:
 // - Write a function to give the elves some tools, or a cup of tea!
 // - Limit the total number of elves to 100.
+
+function addTea() {
+  if (elfZone.childElementCount < 101) {
+    elfZone.innerHTML += `<div class="tea">🍵</div>`
+  }
+}
+
+function addDeer() {
+  if (elfZone.childElementCount < 101) {
+    elfZone.innerHTML += `<div class="deer">🦌</div>`
+  }
+}
