494d5ff05cbc4c7cdc5de5ee977a0b2306ef594a
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-22 17:32:38 +0100

    Basic functionality

--- 22-christmas-stress-buster/index.js
+++ 22-christmas-stress-buster/index.js
@@ -1,7 +1,5 @@
 const btn = document.getElementById("btn")
-let timer = document.getElementById("timer")
-let affirmation = document.getElementById("affirmation")
-let timeout
+const timer = document.getElementById("timer")
 
 // Task:
 // - Write a function to start the countdown timer on the button click and display it in the DOM. Stop the timer when it reaches 0.
@@ -9,3 +7,28 @@ let timeout
 // Stretch goals:
 // - Show a relaxing image in the background while the timer is running.
 // - Play relaxing music.
+
+const startingMinutes = 0.1
+let time = startingMinutes * 60
+
+function updateTimer() {
+  btn.disabled = true
+
+  const countdown = setInterval(() => {
+    let minutes = Math.floor(time / 60)
+    let seconds = time % 60
+
+    minutes = minutes < 10 ? `0${minutes}` : minutes
+    seconds = seconds < 10 ? `0${seconds}` : seconds
+
+    timer.textContent = `${minutes}:${seconds}`
+    time--
+
+    if (time < 0) {
+      clearInterval(countdown)
+      timer.textContent = "You got this!"
+    }
+  }, 1000)
+}
+
+btn.addEventListener("click", updateTimer)
