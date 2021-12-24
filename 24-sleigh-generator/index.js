8b1522a90ec4ff3e6c0f77bed50a5bb538ad307e
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-12-24 11:37:59 +0100

    Challenge setup

---
+++ 24-sleigh-generator/index.js
@@ -0,0 +1,10 @@
+const crew = document.getElementById("crew")
+const btn = document.getElementById("btn")
+
+// Task:
+// - Write a function to launch the sleigh!
+// - See CSS for more tasks.
+
+// Stretch goals:
+// - Add a cheering sound when the sleigh takes off.
+// - Add a countdown to the launch time.
